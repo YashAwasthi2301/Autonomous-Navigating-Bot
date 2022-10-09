4cd2e262bd72315eada75d796d08ba9953df10bf
yashawasthi2301 <yashawasthi50@gmail.com>
2022-10-09 23:38:59 +0530

    first push

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yash/Autonomous-Navigating-Bot/src
+# Build directory: /home/yash/Autonomous-Navigating-Bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot")
